8d93c0a2d52b137fd8953e9086b78fbd00a14fb6
Programmer <peshkov@mbober.ru>
2021-01-25 15:37:30 +0300

    refactor routes file

--- src/routes/roles.php
+++ src/routes/roles.php
@@ -3,28 +3,24 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleRule\RoleController;
 
 Route::group([
-    "middleware" => ["web", "auth", "management"],
+    "middleware" => ["web", "auth", "management", "role-master"],
     "prefix" => "admin",
     "as" => "admin.",
 ], function () {
+    Route::resource("roles", RoleController::class);
     Route::group([
-        "middleware" => ["role-master"],
+        "as" => "roles.",
+        "prefix" => "/roles/{role}",
     ], function () {
-        Route::resource("roles", RoleController::class);
+        Route::get("list/users", [RoleController::class, "users"])
+            ->name("users");
         Route::group([
-            "as" => "roles.",
-            "prefix" => "/roles/{role}",
+            "prefix" => "{rule}"
         ], function () {
-            Route::get("list/users", [RoleController::class, "users"])
-                ->name("users");
-            Route::group([
-                "prefix" => "{rule}"
-            ], function () {
-                Route::get("/", [RoleController::class, "show"])
-                    ->name("rule");
-                Route::put("/", [RoleController::class, "updateRule"]);
-                Route::post("/", [RoleController::class, "defaultRules"]);
-            });
+            Route::get("/", [RoleController::class, "show"])
+                ->name("rule");
+            Route::put("/", [RoleController::class, "updateRule"]);
+            Route::post("/", [RoleController::class, "defaultRules"]);
         });
     });
 });
